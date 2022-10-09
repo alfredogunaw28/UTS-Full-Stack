c1b7ad14815bcd7ed73a5a919d2c26e5a2a06b54
alfredo gunawan <99379271+alfredogunaw28@users.noreply.github.com>
2022-10-09 14:13:37 +0700

    Update inputjadwal.php

--- inputjadwal.php
+++ inputjadwal.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post">
+        <p><label for="">Mahasiswa : 160344232 - Sahara Devaca<?php ?></label>
+            
+        </p>
+        <table border="1" style="min-width: 500px; max-width: 1000px;">
+            <th>jam</th>
+            <th>Minggu</th>
+            <th>Senin</th>
+            <th>Selasa</th>
+            <th>Rabu</th>
+            <th>Kamis</th>
+            <th>Jumat</th>
+            <th>Sabtu</th>
+            <?php
+            for ($i = 0; $i <= 7; $i++) {
+                echo "<tr><td>jam</td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td></tr>";
+            }
+            ?>
+        </table>
+    </form><br>
+    <form method="post" action="inputjadwal.php">
+        <input type="submit" name="btnsimpan" value="simpan">
+    </form>
+</body>
+
+</html>
